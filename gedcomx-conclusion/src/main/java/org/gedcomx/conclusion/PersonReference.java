196b5101d928a3df6513e0bfc89c497aaf9f2e43
Ryan Heaton <heatonra@familysearch.org>
2011-07-01 15:33:41 -0600

    issue #8: remove 'role' on person reference

--- gedcomx-conclusion/src/main/java/org/gedcomx/conclusion/PersonReference.java
+++ gedcomx-conclusion/src/main/java/org/gedcomx/conclusion/PersonReference.java
@@ -15,13 +15,7 @@
  */
 package org.gedcomx.conclusion;
 
-import org.codehaus.enunciate.XmlQNameEnumUtil;
-import org.codehaus.enunciate.qname.XmlQNameEnumRef;
-import org.gedcomx.types.RelationshipRole;
-
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlTransient;
-import javax.xml.namespace.QName;
 import java.net.URI;
 
 /**
@@ -31,47 +25,7 @@ import java.net.URI;
  */
 public final class PersonReference {
 
-  private QName role;
   private URI href;
-
-  /**
-   * The role of the person.
-   *
-   * @return The role of the person.
-   */
-  @XmlAttribute
-  @XmlQNameEnumRef (RelationshipRole.class)
-  public QName getRole() {
-    return role;
-  }
-
-  /**
-   * The role of the person.
-   *
-   * @param role The role of the person.
-   */
-  public void setRole(QName role) {
-    this.role = role;
-  }
-
-  /**
-   * The enum referencing the known role, or {@link org.gedcomx.types.RelationshipRole#other} if not known.
-   *
-   * @return The enum referencing the known role, or {@link org.gedcomx.types.RelationshipRole#other} if not known.
-   */
-  @XmlTransient
-  public RelationshipRole getKnownRole() {
-    return XmlQNameEnumUtil.fromQName(getRole(), RelationshipRole.class);
-  }
-
-  /**
-   * Set the role from a known enumeration of roles.
-   *
-   * @param knownRole The role.
-   */
-  public void setKnownRole(RelationshipRole knownRole) {
-    this.role = XmlQNameEnumUtil.toQName(knownRole);
-  }
 
   /**
    * The link to the person.
