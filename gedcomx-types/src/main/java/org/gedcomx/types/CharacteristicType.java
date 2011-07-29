b5f21c18e4163f3c267e3d8ec7b47d7a20279187
Ryan Heaton <heatonra@familysearch.org>
2011-07-29 11:37:08 -0600

    issue #13: adding a utility method for determining the applicability of a characteristic type

--- gedcomx-types/src/main/java/org/gedcomx/types/CharacteristicType.java
+++ gedcomx-types/src/main/java/org/gedcomx/types/CharacteristicType.java
@@ -20,6 +20,7 @@ import org.codehaus.enunciate.qname.XmlUnknownQNameEnumValue;
 
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.namespace.QName;
+import java.util.EnumSet;
 
 /**
  * Enumeration of standard characteristic types.
@@ -91,61 +92,112 @@ public enum CharacteristicType {
     return org.codehaus.enunciate.XmlQNameEnumUtil.fromQName(qname, CharacteristicType.class);
   }
 
+  /**
+   * Enumeration of types that are applicable to a person characteristic.
+   */
   @XmlTransient
   public static final class Person {
     private Person(){}
 
-    public static final CharacteristicType age = CharacteristicType.age;
-    public static final CharacteristicType caste_name = CharacteristicType.caste_name;
-    public static final CharacteristicType clan_name = CharacteristicType.clan_name;
-    public static final CharacteristicType count_of_children = CharacteristicType.count_of_children;
-    public static final CharacteristicType count_of_marriages = CharacteristicType.count_of_marriages;
-    public static final CharacteristicType died_before_eight = CharacteristicType.died_before_eight;
-    public static final CharacteristicType dwelling = CharacteristicType.dwelling;
-    public static final CharacteristicType ethnicity = CharacteristicType.ethnicity;
-    public static final CharacteristicType gedcom_uuid = CharacteristicType.gedcom_uuid;
-    public static final CharacteristicType household = CharacteristicType.household;
-    public static final CharacteristicType marital_status = CharacteristicType.marital_status;
-    public static final CharacteristicType namesake = CharacteristicType.namesake;
-    public static final CharacteristicType national_id = CharacteristicType.national_id;
-    public static final CharacteristicType national_origin = CharacteristicType.national_origin;
-    public static final CharacteristicType not_accountable = CharacteristicType.not_accountable;
-    public static final CharacteristicType occupation = CharacteristicType.occupation;
-    public static final CharacteristicType never_had_children = CharacteristicType.never_had_children;
-    public static final CharacteristicType never_married = CharacteristicType.never_married;
-    public static final CharacteristicType physical_description = CharacteristicType.physical_description;
-    public static final CharacteristicType possessions = CharacteristicType.possessions;
-    public static final CharacteristicType race = CharacteristicType.race;
-    public static final CharacteristicType religious_affiliation = CharacteristicType.religious_affiliation;
-    public static final CharacteristicType scholastic_achievement = CharacteristicType.scholastic_achievement;
-    public static final CharacteristicType social_security_number = CharacteristicType.social_security_number;
-    public static final CharacteristicType stillborn = CharacteristicType.stillborn;
-    public static final CharacteristicType title_of_nobility = CharacteristicType.title_of_nobility;
-    public static final CharacteristicType tribe_name = CharacteristicType.tribe_name;
-    public static final CharacteristicType twin = CharacteristicType.twin;
+    private static final EnumSet<CharacteristicType> PERSON_CHARACTERISTIC_TYPES = EnumSet.noneOf(CharacteristicType.class);
+    private static CharacteristicType include(CharacteristicType type) {
+      PERSON_CHARACTERISTIC_TYPES.add(type);
+      return type;
+    }
+    
+    public static final CharacteristicType age = include(CharacteristicType.age);
+    public static final CharacteristicType caste_name = include(CharacteristicType.caste_name);
+    public static final CharacteristicType clan_name = include(CharacteristicType.clan_name);
+    public static final CharacteristicType count_of_children = include(CharacteristicType.count_of_children);
+    public static final CharacteristicType count_of_marriages = include(CharacteristicType.count_of_marriages);
+    public static final CharacteristicType died_before_eight = include(CharacteristicType.died_before_eight);
+    public static final CharacteristicType dwelling = include(CharacteristicType.dwelling);
+    public static final CharacteristicType ethnicity = include(CharacteristicType.ethnicity);
+    public static final CharacteristicType gedcom_uuid = include(CharacteristicType.gedcom_uuid);
+    public static final CharacteristicType household = include(CharacteristicType.household);
+    public static final CharacteristicType marital_status = include(CharacteristicType.marital_status);
+    public static final CharacteristicType namesake = include(CharacteristicType.namesake);
+    public static final CharacteristicType national_id = include(CharacteristicType.national_id);
+    public static final CharacteristicType national_origin = include(CharacteristicType.national_origin);
+    public static final CharacteristicType not_accountable = include(CharacteristicType.not_accountable);
+    public static final CharacteristicType occupation = include(CharacteristicType.occupation);
+    public static final CharacteristicType never_had_children = include(CharacteristicType.never_had_children);
+    public static final CharacteristicType never_married = include(CharacteristicType.never_married);
+    public static final CharacteristicType physical_description = include(CharacteristicType.physical_description);
+    public static final CharacteristicType possessions = include(CharacteristicType.possessions);
+    public static final CharacteristicType race = include(CharacteristicType.race);
+    public static final CharacteristicType religious_affiliation = include(CharacteristicType.religious_affiliation);
+    public static final CharacteristicType scholastic_achievement = include(CharacteristicType.scholastic_achievement);
+    public static final CharacteristicType social_security_number = include(CharacteristicType.social_security_number);
+    public static final CharacteristicType stillborn = include(CharacteristicType.stillborn);
+    public static final CharacteristicType title_of_nobility = include(CharacteristicType.title_of_nobility);
+    public static final CharacteristicType tribe_name = include(CharacteristicType.tribe_name);
+    public static final CharacteristicType twin = include(CharacteristicType.twin);
+    
+    /**
+     * Whether the given characteristic type is applicable to a person characteristic.
+     * 
+     * @param type The characteristic type.
+     * @return Whether the given characteristic type is applicable to a person characteristic.
+     */
+    public static boolean isApplicable(CharacteristicType type) {
+      return PERSON_CHARACTERISTIC_TYPES.contains(type);
+    }
   }
 
   @XmlTransient
   public static final class Couple {
     private Couple() {}
 
-    public static final CharacteristicType common_law_marriage = CharacteristicType.common_law_marriage;
-    public static final CharacteristicType universal_id = CharacteristicType.universal_id;
-    public static final CharacteristicType currently_spouses = CharacteristicType.currently_spouses;
-    public static final CharacteristicType never_had_children = CharacteristicType.never_had_children;
-    public static final CharacteristicType never_married = CharacteristicType.never_married;
-    public static final CharacteristicType number_of_children = CharacteristicType.number_of_children;
+    private static final EnumSet<CharacteristicType> COUPLE_CHARACTERISTIC_TYPES = EnumSet.noneOf(CharacteristicType.class);
+    private static CharacteristicType include(CharacteristicType type) {
+      COUPLE_CHARACTERISTIC_TYPES.add(type);
+      return type;
+    }
+    
+    public static final CharacteristicType common_law_marriage = include(CharacteristicType.common_law_marriage);
+    public static final CharacteristicType universal_id = include(CharacteristicType.universal_id);
+    public static final CharacteristicType currently_spouses = include(CharacteristicType.currently_spouses);
+    public static final CharacteristicType never_had_children = include(CharacteristicType.never_had_children);
+    public static final CharacteristicType never_married = include(CharacteristicType.never_married);
+    public static final CharacteristicType number_of_children = include(CharacteristicType.number_of_children);
+
+    /**
+     * Whether the given characteristic type is applicable to a couple characteristic.
+     * 
+     * @param type The characteristic type.
+     * @return Whether the given characteristic type is applicable to a couple characteristic.
+     */
+    public static boolean isApplicable(CharacteristicType type) {
+      return COUPLE_CHARACTERISTIC_TYPES.contains(type);
+    }
   }
 
   @XmlTransient
   public static final class ParentChild {
     private ParentChild() {}
 
-    public static final CharacteristicType biological = CharacteristicType.biological;
-    public static final CharacteristicType adopted = CharacteristicType.adopted;
-    public static final CharacteristicType step = CharacteristicType.step;
-    public static final CharacteristicType foster = CharacteristicType.foster;
-    public static final CharacteristicType guardianship = CharacteristicType.guardianship;
+    private static final EnumSet<CharacteristicType> PARENT_CHILD_CHARACTERISTIC_TYPES = EnumSet.noneOf(CharacteristicType.class);
+    private static CharacteristicType include(CharacteristicType type) {
+      PARENT_CHILD_CHARACTERISTIC_TYPES.add(type);
+      return type;
+    }
+    
+    public static final CharacteristicType biological = include(CharacteristicType.biological);
+    public static final CharacteristicType adopted = include(CharacteristicType.adopted);
+    public static final CharacteristicType step = include(CharacteristicType.step);
+    public static final CharacteristicType foster = include(CharacteristicType.foster);
+    public static final CharacteristicType guardianship = include(CharacteristicType.guardianship);
+
+    /**
+     * Whether the given characteristic type is applicable to a ParentChild characteristic.
+     * 
+     * @param type The characteristic type.
+     * @return Whether the given characteristic type is applicable to a ParentChild characteristic.
+     */
+    public static boolean isApplicable(CharacteristicType type) {
+      return PARENT_CHILD_CHARACTERISTIC_TYPES.contains(type);
+    }
   }
 
 }
