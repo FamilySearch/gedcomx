2ed87e49f2640ea253beaa207546ac8bc3b47d48
Ryan Heaton <ryan@webcohesion.com>
2011-10-25 15:49:11 -0600

    initial class, namespace setup

---
+++ conclusion-tree-model/src/main/java/org/familysearch/ct/package-info.java
@@ -0,0 +1,13 @@
+
+@XmlSchema (
+  namespace = ConclusionTreeNamespaces.CONCLUSION_TREE_V1_NAMESPACE,
+  attributeFormDefault = XmlNsForm.QUALIFIED,
+  elementFormDefault = XmlNsForm.QUALIFIED
+)
+@XmlAccessorOrder ( XmlAccessOrder.ALPHABETICAL )
+package org.familysearch.ct;
+
+import javax.xml.bind.annotation.XmlAccessOrder;
+import javax.xml.bind.annotation.XmlAccessorOrder;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
