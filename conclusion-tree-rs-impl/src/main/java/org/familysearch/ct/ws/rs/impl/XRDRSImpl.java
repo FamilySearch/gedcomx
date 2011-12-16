39148579c2a572204041d5c4d509acefcb609be0
mgardiner <gardinerm@familysearch.org>
2011-12-16 09:58:45 -0700

    Remove unused import.

--- conclusion-tree-rs-impl/src/main/java/org/familysearch/ct/ws/rs/impl/XRDRSImpl.java
+++ conclusion-tree-rs-impl/src/main/java/org/familysearch/ct/ws/rs/impl/XRDRSImpl.java
@@ -11,7 +11,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 /**
  * @author Mike Gardiner
