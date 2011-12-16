7cdbb929c6fb27947f791c4860ee1025a24584ba
Ryan Heaton <ryan@webcohesion.com>
2011-12-16 15:52:55 -0700

    adjusting the path to better conform to the [host-meta](tools.ietf.org/html/draft-hammer-hostmeta) spec

--- conclusion-tree-rs-impl/src/main/java/org/familysearch/ct/ws/rs/impl/XRDRSImpl.java
+++ conclusion-tree-rs-impl/src/main/java/org/familysearch/ct/ws/rs/impl/XRDRSImpl.java
@@ -18,72 +18,43 @@ import java.util.Date;
 /**
  * @author Mike Gardiner
  */
-@Path("/discover")
+@Path ( "/.well-known/host-meta" )
 public class XRDRSImpl implements XRDRSDefinition {
-    @Context
-    UriInfo uriInfo;
-    Date startupDate =  Calendar.getInstance().getTime();
 
-    @GET
-    @Override
-    public Response readXRD() {
+  @Context
+  UriInfo uriInfo;
+  Date startupDate =  Calendar.getInstance().getTime();
 
-        try {
-            return Response.ok(buildXRD()).build();
-        }
-        catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
-    }
+  @GET
+  @Override
+  public Response readXRD() {
+    return Response.ok(buildXRD()).build();
+  }
 
-    @HEAD
-    @Override
-    public Response headXRD() {
-        return Response.ok().lastModified(startupDate).build();
-    }
+  @HEAD
+  @Override
+  public Response headXRD() {
+    return Response.ok().lastModified(startupDate).build();
+  }
 
-    @Override
-    public void setProofStatement(String proofStatement) {
+  @Override
+  public void setProofStatement(String proofStatement) {
 
-    }
+  }
 
-    protected XRD buildXRD() throws Exception {
-        String basePath = uriInfo.getBaseUri().getPath();
-        XRD xrd = new XRD();
-        xrd.setSubject(new URI(basePath));
+  protected XRD buildXRD() {
+    String basePath = uriInfo.getBaseUri().getPath();
+    XRD xrd = new XRD();
+    xrd.setSubject(URI.create(basePath));
 
-        // Persons Link
-        Link personsLink = new Link();
-        personsLink.setHref(new URI(basePath + "/persons"));
-        Title personsTitle = new Title();
-        personsTitle.setValue("Persons");
-        personsLink.getTitles().add(personsTitle);
-        xrd.getLinks().add(personsLink);
+    // Persons Link
+    Link personsLink = new Link();
+    personsLink.setHref(URI.create(basePath + "/persons"));
+    Title personsTitle = new Title();
+    personsTitle.setValue("Persons");
+    personsLink.getTitles().add(personsTitle);
+    xrd.getLinks().add(personsLink);
 
-        // Person Link
-        Link personLink = new Link();
-        personLink.setHref(new URI(basePath + "/person/{rid}"));
-        Title personTitle = new Title();
-        personTitle.setValue("Person");
-        personLink.getTitles().add(personTitle);
-        xrd.getLinks().add(personLink);
-
-        // Relationships Link
-        Link relationshipsLink = new Link();
-        relationshipsLink.setHref(new URI(relationshipsLink + "/relationships"));
-        Title relationshipsTitle = new Title();
-        relationshipsTitle.setValue("Relationships");
-        relationshipsLink.getTitles().add(relationshipsTitle);
-        xrd.getLinks().add(relationshipsLink);
-
-        // Relationship Link
-        Link relationshipLink = new Link();
-        relationshipLink.setHref(new URI(relationshipLink + "/relationship/{rid}"));
-        Title relationshipTitle = new Title();
-        relationshipTitle.setValue("Relationship");
-        relationshipLink.getTitles().add(relationshipTitle);
-        xrd.getLinks().add(relationshipLink);
-
-        return xrd;
-    }
+    return xrd;
+  }
 }
