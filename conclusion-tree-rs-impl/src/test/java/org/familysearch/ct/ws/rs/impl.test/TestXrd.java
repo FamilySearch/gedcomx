1fdbf8f6cb16eb9fa31fd05415c611b79caa54f7
mgardiner <gardinerm@familysearch.org>
2011-12-19 14:02:57 -0700

    Finish adding asserts for TestXrd test.

--- conclusion-tree-rs-impl/src/test/java/org/familysearch/ct/ws/rs/impl.test/TestXrd.java
+++ conclusion-tree-rs-impl/src/test/java/org/familysearch/ct/ws/rs/impl.test/TestXrd.java
@@ -1,10 +1,14 @@
 package org.familysearch.ct.ws.rs.impl.test;
 
 import org.familysearch.ct.ws.rs.impl.XrdRSImpl;
+import org.gedcomx.xrd.Link;
+import org.gedcomx.xrd.Title;
 import org.gedcomx.xrd.XRD;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.core.Response;
+
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
@@ -22,6 +26,18 @@ public class TestXrd {
         XRD xrd = (XRD) response.getEntity();
         assertNotNull(xrd);
         assertNull(xrd.getSubject());
-        assertEquals(1, xrd.getLinks().size());
+
+        List<Link> links = xrd.getLinks();
+        assertNotNull(links);
+        assertEquals(1, links.size());
+
+        Link personLink = links.get(0);
+        assertNotNull(personLink);
+        assertEquals("persons", personLink.getHref().getPath());
+
+        assertNotNull(personLink.getTitles());
+        assertEquals(1, personLink.getTitles().size());
+        Title personTitle = personLink.getTitles().get(0);
+        assertEquals("Persons", personTitle.getValue());
     }
 }
