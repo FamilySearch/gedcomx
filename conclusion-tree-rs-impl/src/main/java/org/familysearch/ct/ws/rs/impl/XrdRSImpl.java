a928553c6cb5e77608062d86a4dce021efe46e07
Ryan Heaton <ryan@webcohesion.com>
2011-12-19 12:11:42 -0700

    npe in xrd resource impl

--- conclusion-tree-rs-impl/src/main/java/org/familysearch/ct/ws/rs/impl/XrdRSImpl.java
+++ conclusion-tree-rs-impl/src/main/java/org/familysearch/ct/ws/rs/impl/XrdRSImpl.java
@@ -13,49 +13,52 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 /**
  * @author Mike Gardiner
  */
-@Path("/.well-known/host-meta")
-@Produces("application/xrd+xml")
+@Path ( "/.well-known/host-meta" )
+@Produces ( "application/xrd+xml" )
 public class XrdRSImpl implements XrdRSDefinition {
-    @Context
-    UriInfo uriInfo;
-    Date startupDate = Calendar.getInstance().getTime();
+  
+  @Context
+  UriInfo uriInfo;
+  Date startupDate = Calendar.getInstance().getTime();
 
-    @GET
-    @Override
-    public Response readXRD() {
-        return Response.ok(buildXRD()).build();
-    }
+  @GET
+  @Override
+  public Response readXRD() {
+    return Response.ok(buildXRD()).build();
+  }
 
-    @HEAD
-    @Override
-    public Response headXRD() {
-        return Response.ok().lastModified(startupDate).build();
-    }
+  @HEAD
+  @Override
+  public Response headXRD() {
+    return Response.ok().lastModified(startupDate).build();
+  }
 
-    @Override
-    public void setProofStatement(String proofStatement) {
+  @Override
+  public void setProofStatement(String proofStatement) {
 
-    }
+  }
 
-    protected XRD buildXRD() {
-        String basePath = uriInfo.getBaseUri().getPath();
-        XRD xrd = new XRD();
-        xrd.setSubject(URI.create(basePath));
+  protected XRD buildXRD() {
+    String basePath = uriInfo.getBaseUri().getPath();
+    XRD xrd = new XRD();
+    xrd.setLinks(new ArrayList<Link>());
+    xrd.setSubject(URI.create(basePath));
 
-        // Persons Link
-        Link personsLink = new Link();
-        personsLink.setHref(URI.create(basePath + "/persons"));
-        Title personsTitle = new Title();
-        personsTitle.setValue("Persons");
-        personsLink.getTitles().add(personsTitle);
-        xrd.getLinks().add(personsLink);
+    // Persons Link
+    Link personsLink = new Link();
+    personsLink.setHref(URI.create(basePath + "/persons"));
+    Title personsTitle = new Title();
+    personsTitle.setValue("Persons");
+    personsLink.getTitles().add(personsTitle);
+    xrd.getLinks().add(personsLink);
 
-        return xrd;
-    }
+    return xrd;
+  }
 }
