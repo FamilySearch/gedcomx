bbb35401d085f9755f395d87790a6796fdbd1858
mgardiner <gardinerm@familysearch.org>
2012-01-05 14:36:03 -0700

    Added javadoc to DocAwareJerseyTest

--- conclusion-tree-test-support/src/main/java/org/familysearch/ct/ws/test/DocAwareJerseyTest.java
+++ conclusion-tree-test-support/src/main/java/org/familysearch/ct/ws/test/DocAwareJerseyTest.java
@@ -18,6 +18,12 @@ import java.io.OutputStream;
 import java.net.URI;
 
 /**
+ * This class is used to document RESTful resources by describing uses cases for a given
+ * request and response to a RESTful resource.  It depends of JUnit 4 and the Jersey test
+ * framework. Use case documentation files are written to the configured target location
+ * and the format of the filename is [some-title].usecase.xml.  By default the files will
+ * be put into the target/generated-doc directory.
+ *
  * @author Mike Gardiner and Ryan Heaton
  */
 public class DocAwareJerseyTest extends JerseyTest {
@@ -26,21 +32,47 @@ public class DocAwareJerseyTest extends JerseyTest {
 
   protected UseCaseLoggingFilter filter;
 
+  /**
+   * Default constructor
+   *
+   * @throws TestContainerException - An error occurred
+   */
   public DocAwareJerseyTest() throws TestContainerException {
   }
 
+  /**
+   * Constructor
+   *
+   * @param testContainerFactory - A test container factory to use
+   */
   public DocAwareJerseyTest(TestContainerFactory testContainerFactory) {
     super(testContainerFactory);
   }
 
+  /**
+   * Constructor
+   *
+   * @param ad - The application descriptor to use
+   * @throws TestContainerException - An error occurred
+   */
   public DocAwareJerseyTest(AppDescriptor ad) throws TestContainerException {
     super(ad);
   }
 
+  /**
+   * Constructor
+   *
+   * @param packages - Packages to scan for resources
+   * @throws TestContainerException - An error occurred
+   */
   public DocAwareJerseyTest(String... packages) throws TestContainerException {
     super(packages);
   }
 
+  /**
+   * @return The test container factory
+   * @throws TestContainerException - An error occurred
+   */
   @Override
   protected TestContainerFactory getTestContainerFactory() throws TestContainerException {
     return new InMemoryTestContainerFactory() {
@@ -53,6 +85,13 @@ public class DocAwareJerseyTest extends JerseyTest {
     };
   }
 
+  /**
+   * Get the client
+   *
+   * @param tc - Test container
+   * @param ad - Application descriptor
+   * @return Client
+   */
   @Override
   protected Client getClient(TestContainer tc, AppDescriptor ad) {
     this.filter = new UseCaseLoggingFilter();
@@ -62,6 +101,12 @@ public class DocAwareJerseyTest extends JerseyTest {
     return client;
   }
 
+  /**
+   * Create a new use case
+   *
+   * @param title - Title of the use case
+   * @return UseCase
+   */
   protected UseCase createUseCase(String title) {
     if (title == null || title.isEmpty()) {
       throw new IllegalArgumentException("title must not be null or empty");
@@ -73,10 +118,21 @@ public class DocAwareJerseyTest extends JerseyTest {
     return uc;
   }
 
+  /**
+   * Sets a description for the use case
+   *
+   * @param description - A description for the use case
+   */
   protected void setRequestDescription(String description) {
     this.filter.setCurrentRequestDescription(description);
   }
 
+  /**
+   * This is where the use case files are created containing the
+   * request and response information.
+   *
+   * @throws Exception
+   */
   @Override
   public void tearDown() throws Exception {
     super.tearDown();
@@ -105,6 +161,12 @@ public class DocAwareJerseyTest extends JerseyTest {
     }
   }
 
+  /**
+   * Generate a new filename based on the title of the use case
+   *
+   * @param title - The title to use
+   * @return String representing the filename
+   */
   private String generateFilename(String title) {
     StringBuilder filename = new StringBuilder(title.replace(' ', '-'));
     filename.append(".");
@@ -112,6 +174,9 @@ public class DocAwareJerseyTest extends JerseyTest {
     return filename.toString();
   }
 
+  /**
+   * Marks the use case as ended
+   */
   protected void endUseCase() {
     this.filter.setCurrentUseCase(null);
   }
