20449f0855d671bce66216cd820915a5c1eab128
mgardiner <gardinerm@familysearch.org>
2012-01-05 14:48:58 -0700

    Updated javadoc in FormattingJAXBContextResolver.

--- conclusion-tree-test-support/src/main/java/org/familysearch/ct/ws/test/FormattingJAXBContextResolver.java
+++ conclusion-tree-test-support/src/main/java/org/familysearch/ct/ws/test/FormattingJAXBContextResolver.java
@@ -5,11 +5,19 @@ import javax.ws.rs.ext.Provider;
 import javax.xml.bind.*;
 
 /**
+ * This class is used to configure the body format when marshaling objects.
+ *
  * @author Ryan Heaton
  */
 @Provider
 public class FormattingJAXBContextResolver implements ContextResolver<JAXBContext> {
 
+  /**
+   * Gets a JAXB context
+   *
+   * @param type - The class type
+   * @return JAXBContext
+   */
   @Override
   public JAXBContext getContext(Class<?> type) {
     try {
@@ -20,20 +28,39 @@ public class FormattingJAXBContextResolver implements ContextResolver<JAXBContex
     }
   }
 
+  /**
+   * Inner class to override formatting configuration
+   */
   private static final class FormattingJAXBContext extends JAXBContext {
 
     private final JAXBContext delegate;
 
-
+    /**
+     * Constructor
+     *
+     * @param delegate - The JAXB context to use
+     */
     public FormattingJAXBContext(JAXBContext delegate) {
       this.delegate = delegate;
     }
 
+    /**
+     * Creates an unmarshaller
+     *
+     * @return Unmarshaller
+     * @throws JAXBException
+     */
     @Override
     public Unmarshaller createUnmarshaller() throws JAXBException {
       return delegate.createUnmarshaller();
     }
 
+    /**
+     * Creates a marshaller
+     *
+     * @return Marshaller
+     * @throws JAXBException
+     */
     @Override
     public Marshaller createMarshaller() throws JAXBException {
       Marshaller marshaller = delegate.createMarshaller();
@@ -41,6 +68,12 @@ public class FormattingJAXBContextResolver implements ContextResolver<JAXBContex
       return marshaller;
     }
 
+    /**
+     * Creates a validator
+     *
+     * @return Validator
+     * @throws JAXBException
+     */
     @Override
     public Validator createValidator() throws JAXBException {
       return delegate.createValidator();
