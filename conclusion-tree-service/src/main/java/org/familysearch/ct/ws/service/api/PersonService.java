f37a2e98eff15f89c9070ebdc37541334d35a056
Ryan Heaton <ryan@webcohesion.com>
2012-01-12 12:00:17 -0700

    removing superfluous methods on the person service

--- conclusion-tree-service/src/main/java/org/familysearch/ct/ws/service/api/PersonService.java
+++ conclusion-tree-service/src/main/java/org/familysearch/ct/ws/service/api/PersonService.java
@@ -1,6 +1,5 @@
 package org.familysearch.ct.ws.service.api;
 
-import java.util.Date;
 import org.gedcomx.conclusion.Person;
 
 /**
@@ -16,21 +15,5 @@ public interface PersonService {
    * @return The entity bundle, or null if the summary was not found.
    */
   public EntityBundle<Person> getPersonSummary(String personId);
-
-  /**
-   * Get the timestamp of the summary for the specified person.
-   *
-   * @param personId The id of the person.
-   * @return The last modified timestamp of the summary, or null if
-   */
-  public Date getSummaryLastModified(String personId);
-
-  /**
-   * Get the person summary
-   *
-   * @param personId The id of the person for which to read the summary.
-   * @return The person summary.
-   */
-  public Person getSummary(String personId);
 
 }
