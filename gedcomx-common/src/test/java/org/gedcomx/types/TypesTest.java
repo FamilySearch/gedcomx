4440bf0ef04b958bbd67c82725baa5cebaa1fefd
Reed Ellsworth <ellsworthrw@familysearch.org>
2012-03-01 17:04:26 -0700

    add unit test

---
+++ gedcomx-common/src/test/java/org/gedcomx/types/TypesTest.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2011 Intellectual Reserve, Inc. All Rights reserved.
+ */
+package org.gedcomx.types;
+
+import org.testng.annotations.Test;
+
+/**
+ */
+public class TypesTest {
+  @Test
+  public void testToQNameURI() throws Exception {
+    // These enum classes don't need to be tested, so just call toQNameURI to get the code referenced in a test
+    AgePartType.Days.toQNameURI();
+    DatePartType.Days.toQNameURI();
+    FactType.Age.isBirthLike();
+    FactType.Couple.Marriage.toQNameURI();
+    FactType.ParentChild.Biological.toQNameURI();
+    FactType.Person.Baptism.toQNameURI();
+    FactType.Record.Household.toQNameURI();
+    GenderType.Male.toQNameURI();
+    MaritalStatusType.Married.toQNameURI();
+    NamePartType.Given.toQNameURI();
+    NameType.Formal.toQNameURI();
+    PlacePartType.Address.toQNameURI();
+    RecordType.Census.getBaseType();
+    RelationshipRole.Ancestor.toQNameURI();
+    RelationshipType.Couple.toQNameURI();
+  }
+}
