80a390c1f4773b41318dacf02ee741371f192e11
dchamberlain <chamberlaindb@familysearch.org>
2012-05-24 13:45:53 -0600

    Added Arrival fact type. Changed PortOfDeparture fact type to just Departure.

--- gedcomx-common/src/main/java/org/gedcomx/types/FactType.java
+++ gedcomx-common/src/main/java/org/gedcomx/types/FactType.java
@@ -37,6 +37,7 @@ public enum FactType {
   Adoption,
   AdultChristening,
   Affiliation,
+  Arrival,
   Baptism,
   BarMitzvah,
   BatMitzvah,
@@ -54,6 +55,7 @@ public enum FactType {
   CountOfMarriages,
   Cremation,
   Death,
+  Departure,
   DiedBeforeEight,
   Emigration,
   Ethnicity,
@@ -89,7 +91,6 @@ public enum FactType {
   Ordinance,
   Ordination,
   PhysicalDescription,
-  PortOfDeparture,
   PreviousResidence,
   Probate,
   Possessions,
@@ -202,6 +203,7 @@ public enum FactType {
     public static final FactType Adoption = include(FactType.Adoption);
     public static final FactType AdultChristening = include(FactType.AdultChristening);
     public static final FactType Affiliation = include(FactType.Affiliation);
+    public static final FactType Arrival = include(FactType.Arrival);
     public static final FactType Baptism = include(FactType.Baptism);
     public static final FactType BarMitzvah = include(FactType.BarMitzvah);
     public static final FactType BatMitzvah = include(FactType.BatMitzvah);
@@ -219,6 +221,7 @@ public enum FactType {
     public static final FactType CountOfMarriages = include(FactType.CountOfMarriages);
     public static final FactType Cremation = include(FactType.Cremation);
     public static final FactType Death = include(FactType.Death);
+    public static final FactType Departure = include(FactType.Departure);
     public static final FactType DiedBeforeEight = include(FactType.DiedBeforeEight);
     public static final FactType Emigration = include(FactType.Emigration);
     public static final FactType Ethnicity = include(FactType.Ethnicity);
@@ -256,7 +259,6 @@ public enum FactType {
     public static final FactType Ordination = include(FactType.Ordination);
     public static final FactType PhysicalDescription = include(FactType.PhysicalDescription);
     public static final FactType Probate = include(FactType.Probate);
-    public static final FactType PortOfDeparture = include(FactType.PortOfDeparture);
     public static final FactType Possessions = include(FactType.Possessions);
     public static final FactType PreviousResidence = include(FactType.PreviousResidence);
     public static final FactType Race = include(FactType.Race);
