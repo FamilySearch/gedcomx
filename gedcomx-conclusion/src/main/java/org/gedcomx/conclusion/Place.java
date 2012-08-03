ef373dd9755dd5e390dc8686f4d564fc6e5cf8aa
Reed Ellsworth <ellsworthrw@familysearch.org>
2012-08-03 17:02:11 -0600

    person trashbin resource

--- gedcomx-conclusion/src/main/java/org/gedcomx/conclusion/Place.java
+++ gedcomx-conclusion/src/main/java/org/gedcomx/conclusion/Place.java
@@ -72,4 +72,9 @@ public final class Place implements Formalizeable {
   public void setFormal(FormalValue formal) {
     this.formal = formal;
   }
+
+  @Override
+  public String toString() {
+    return "Place{" + "original='" + original + '\'' + ", formal=" + formal + '}';
+  }
 }
